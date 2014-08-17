c3c5a94148a6fcae495a268a6ab4bf246c988d5e
Raghav Chidella <nishragh@Raghavs-MacBook-Pro.local>
2014-08-17 14:46:18 -0700

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+console.log("Request reached to the website");
+
 /* GET home page. */
 router.get('/', function(req, res) {
+  console.log("Into the main");
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+  console.log("Request for login");
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
